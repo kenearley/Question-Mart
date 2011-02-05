4e91561ba36950f77478e05f0b845e7fd2fba93d
Ken Earley <ken.earley@sentientbit.com>
2011-02-05 14:04:05 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Questionmart::Application.config.secret_token = '7a93b86bcd9054cae41bb2075d1b5494d316340f962215fde139ba1de2913e35fce9842bc9c27391cc5ac70d9cb1b8b243c1bb96f2202ad954775a69890d7484'
